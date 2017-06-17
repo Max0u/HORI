6f66d72b7b855cd33c7ca9e9cdba5408b6375cc7
Maxime Blanc <maxime2.blanc@epita.fr>
2017-06-17 10:34:43 +0200

    Update TextAnalyzerService.cs

--- viva/TlDr.Core/TextAnalyzerService.cs
+++ viva/TlDr.Core/TextAnalyzerService.cs
@@ -1,6 +1,10 @@
-﻿using System;
+using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
+using Microsoft.ProjectOxford.Text.Topic;
+using Microsoft.ProjectOxford.Text.KeyPhrase;
+using Newtonsoft.Json;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,8 +12,6 @@ namespace TlDr.Core
 {
     public class TextAnalyzerService
     {
-        public delegate void AnalyzedEvent(object sender, bool succed);
-        public static event AnalyzedEvent Analyzed;
 
         /// <summary>
         /// Faire le matching entre le fileNameTxt ( = resultat du speetchtotext) et le fichier WaveName + Personne 
@@ -26,9 +28,58 @@ namespace TlDr.Core
         /// <param name="fileNameTxt"></param>
         /// <param name="fileNameWaveJson"></param>
         /// <returns></returns>
-        public static Task Start(string fileNameTxt, object fileNameWaveJson)
+        /// 
+        public static void Start(string fileName)
         {
-            throw new NotImplementedException();
+            string txtJson = "";
+            if (File.Exists(fileName))
+            {
+                string json = File.ReadAllText(fileName);
+                List<Script> jsonFromTxt = JsonConvert.DeserializeObject<List<Script>>(json);
+                foreach (var item in jsonFromTxt)
+                {
+                   txtJson += item.Sentence;
+                }
+                List<string> arr = TextAnalyzerService.KeyWord(txtJson);
+            }
+
+        }
+        
+        public static List<string> KeyWord(string txt)
+        {
+            Console.WriteLine(txt);
+            List<string> arr = new List<string>();
+            var apiKey = "3f99cd2db7e8405c85fced07a31756d5";
+
+            var document = new KeyPhraseDocument()
+            {
+                Id = "a026ef6b-9136-4935-a428-6258ebdc87f1",
+                Text = txt,
+                Language = "en"
+            };
+
+            var request = new KeyPhraseRequest();
+            request.Documents.Add(document);
+
+            var client = new KeyPhraseClient(apiKey);
+
+            var response = client.GetKeyPhrases(request);
+
+            foreach (var doc in response.Documents)
+            {
+                //Console.WriteLine("Document Id: {0}", doc.Id);
+
+                foreach (var keyPhrase in doc.KeyPhrases)
+                {
+                    arr.Add(keyPhrase);
+                }
+                //Console.WriteLine();
+            }
+            
+
+
+            return arr;
         }
     }
 }
+
